c25c3f875f1a2576cea7ab89d182ab3f879641d8
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2021-02-07 15:23:13 -0600

    Establish query structure with test functions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 });
 
 const choices = [
-    'View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role']
+    'View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit']
 
 const action = [
     {
@@ -31,9 +31,109 @@ connection.connect((err) => {
 const init = () => {
     inquirer.prompt(action)
         .then(answer => {
-            connection.query(`SELECT * FROM employee`, (err, res) => {
-                if (err) console.log(err)
-                console.table(res)
-            })
+            switch(answer.action) {
+                case choices[0]:
+                    viewDepartments();
+                    break;
+
+                case choices[1]:
+                    viewRoles();
+                    break;
+
+                case choices[2]:
+                    viewEmployees();
+                    break;
+
+                case choices[3]:
+                    addDepartment();
+                    break;
+
+                case choices[4]:
+                    addRole();
+                    break;
+
+                case choices[5]:
+                    addEmployee();
+                    break;
+
+                case choices[6]:
+                    updateEmployee();
+                    break;
+
+                case choices[7]:
+                    exit();
+                    break;   
+            }
         })
+}
+
+
+const viewDepartments = () => {
+    connection.query('SELECT * FROM department', (err, res) => {
+        if (err) console.log(err);
+        console.table('Departments', res);
+        anotherAction();
+        
+    })
+}
+
+const viewRoles = () => {
+    connection.query('SELECT * FROM emp_role', (err, res) => {
+        if (err) console.log(err);
+        console.table('Roles', res);
+        anotherAction();
+    })
+}
+
+const viewEmployees = () => {
+    connection.query('SELECT * FROM employee', (err, res) => {
+        if (err) console.log(err);
+        console.table('Employees', res);
+        anotherAction();
+    })
+}
+
+const addDepartment = () => {
+    console.log('addDepartment');
+    anotherAction();
+}
+
+const addRole = () => {
+    console.log('addRole');
+    anotherAction();
+}
+
+const addEmployee = () => {
+    console.log('addEmployee');
+    anotherAction();
+}
+
+const updateEmployee = () => {
+    console.log('updateEmployee');
+    anotherAction();
+}
+
+const anotherAction = () => {
+    inquirer.prompt(
+        [
+            {
+                type: 'list',
+                name: 'continue',
+                message: 'What would you like to do something else?',
+                choices: ['Yes', 'No']   
+            }
+        ]
+        )
+        .then(answer => {
+            if (answer.continue === 'Yes') {
+                init()
+            }else{
+                exit()
+            }
+        })
+}
+
+const exit = () => {
+    connection.end();
+    process.exit();
 }
